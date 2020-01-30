631fbe596c67bae7992bf65d487a46b9ac2e0dec
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-30 09:08:59 +0100

    time

--- handelbars/js/script.js
+++ handelbars/js/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
   $('.invia').click(function() {
     var textNew = $('.send-text').val();
+    var data = new Date();
+    var hours = data.getHours();
+    var minutes = data.getMinutes();
+    var time = hours + ': ' + minutes;
 
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
 
     var context = {
-      'text' : textNew
+      'text' : textNew,
+      'time' : time
     };
     var html = template(context);
     $('#app').append(html);
